7c7e5e7a8aca49e91355bdfab888f4728bdaaf2c
Nap Joseph Calub <njncalub@gmail.com>
2018-06-02 09:50:46 +0800

    Used scientific notation for 10^9 (1e9) and multiplied it to the time.Second object to calculate the gigasecond. AddGigasecond now passes the unit tests.

--- gigasecond/gigasecond.go
+++ gigasecond/gigasecond.go
@@ -1,18 +1,10 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package gigasecond should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package gigasecond calculates the moment when someone has lived for
+// 1 gigasecond. A gigasecond is 10^9 (1,000,000,000) seconds.
 package gigasecond
 
-// import path for the time package from the standard library
 import "time"
 
-// AddGigasecond should have a comment documenting it.
+// AddGigasecond adds 10^9 seconds to the passed Time and returns it.
 func AddGigasecond(t time.Time) time.Time {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return t
+	return t.Add(time.Second * 1e9)
 }
